dc26a8123190551bd884446dec39744cf662463a
406programmer <Sourabh71234@gmail.com>
2026-03-15 18:36:05 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,30 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+dotenv.config();
 
-const app=express()
-const _dirname = path.resolve()
-const PORT=process.env.PORT||3001
+const app = express();
+const _dirname = path.resolve();
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+const PORT = process.env.PORT || 3001;
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
 }
 
-app.get("*",(req,res)=>res.sendFile(path.join(_dirname,"../frontend/dist/index.html")))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(_dirname, "../frontend/dist/index.html")),
+);
 
-app.listen(PORT,()=>{   
-    console.log('server running... on port',PORT)
-})
+app.listen(PORT, () => {
+  console.log("server running... on port", PORT);
+  connectDB();
+});
