8d7a0208e727cfae2c8f7bd954389a9a945d87cd
406programmer <Sourabh71234@gmail.com>
2026-03-16 10:20:51 +0530

    login and logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../../emails/emailHandlers.js";
-import ENV from "../lib/env.js"
+import ENV from "../lib/env.js";
 
 export async function signup(req, res) {
   const { fullName, email, password } = req.body;
@@ -34,24 +34,53 @@ export async function signup(req, res) {
 
     const savedUser = await newUser.save();
     generateToken(savedUser._id, res);
-   
-     res.status(201).json({
+
+    res.status(201).json({
       _id: savedUser._id,
       fullName: savedUser.fullName,
       email: savedUser.email,
       profilePic: savedUser.profilePic,
     });
-    
-    try {
-      await sendWelcomeEmail(savedUser.email,savedUser.fullName,ENV.CLIENT_URL)
-      console.log('Email sent successfully!');
-      
-    } catch (error) {
-      console.error("Failed to send welcome email:",error)
-    }
 
+    try {
+      await sendWelcomeEmail(
+        savedUser.email,
+        savedUser.fullName,
+        ENV.CLIENT_URL,
+      );
+      console.log("Email sent successfully!");
+    } catch (error) {
+      console.error("Failed to send welcome email:", error);
+    }
   } catch (error) {
     console.log("Error in signup controller:", error);
     res.status(500).json({ message: "Internal server error" });
   }
+}
+export async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    if(!email || !password) return res.status(400).json({message : "Invalid Credentials"})
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials" });
+    generateToken(user._id,res)
+    res.status(200).json({
+      _id:user._id,
+      fullName:user.fullName,
+      email:user.email,
+      profilePic:user.profilePic
+    })
+  } catch (error) {
+    console.error("Error in login controller :",error)
+    res.status(500).json({message:"internal server error"})
+  }
+}
+export async function logout(_, res) {
+  res.cookie("jwt","",{
+ maxAge:0,
+  })
+  res.status(200).json({message : "Logged out successfully"})
 }
