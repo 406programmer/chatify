eee663da30f87f60f23d5b6078dca3d3fad3277b
406programmer <Sourabh71234@gmail.com>
2026-03-18 20:45:50 +0530

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ENV from "./lib/env.js"
 import path from "path";
+import cors from "cors";
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +13,7 @@ const _dirname = path.resolve();
 const PORT = ENV.PORT || 3001;
 
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
