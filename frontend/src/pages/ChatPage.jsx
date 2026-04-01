67fa74778e904db8e0a96d6f7f2407ae75d6f8ab
406programmer <Sourabh71234@gmail.com>
2026-04-01 19:30:59 +0530

    responsive design implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -15,11 +15,11 @@ function ChatPage() {
     <div className="relative w-full max-w-6xl h-[700px]">
       <BorderAnimatedContainer>
         {/* LEFT SIDE */}
-        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+        <div className={`lg:w-80 w-full ${selectedUser ? "hidden" : ""} bg-slate-800/50 backdrop-blur-sm flex flex-col`}>
           <ProfileHeader />
           <ActiveTabSwitch />
 
-          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+          <div className={`flex-1  overflow-y-auto p-4 space-y-2`}>
             {activeTab === "chats" ? <ChatsList /> : <ContactList />}
           </div>
         </div>
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -21,7 +21,7 @@ export const useAuthStore = create((set,get) => ({
 
     } catch (error) {
       console.log("Error in authCheck", error);
-      toast.error(error.response?.data?.message || "Authentication check failed");
+      // toast.error(error.response?.data?.message);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
